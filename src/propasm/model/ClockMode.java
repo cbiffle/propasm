28408a39be69bc79e326245eeb4de65b6141827f
cbiffle <cbiffle@ec6df2ae-cf20-0410-9651-818c09fd334d>
2007-06-05 21:16:46 +0000

    Added in the clock config model (accidentally omitted from the last rev).

---
+++ src/propasm/model/ClockMode.java
@@ -0,0 +1,64 @@
+// Copyright (C) 2006 Cliff L. Biffle.
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+package propasm.model;
+
+
+/**
+ * Describes the various possible settings for the P8X32's clock.
+ * 
+ * @author cbiffle
+ *
+ */
+public enum ClockMode {
+  XINPUT(0x22, true),
+  XTAL1( 0x2A, true),
+  XTAL2( 0x32, true),
+  XTAL3( 0x3A, true),
+  RCFAST(0x00, false) {
+    @Override public int derive(int inputFrequency) {
+      return 12000000;
+    }
+  },
+  RCSLOW(0x01, false) {
+    @Override public int derive(int inputFrequency) {
+      return 20000;
+    }
+  };
+  
+  private final byte base;
+  private final boolean external;
+ 
+  private ClockMode(int base, boolean external) {
+    this.base = (byte)base;
+    this.external = external;
+  }
+  
+  public byte getValue(PllMode pll) {
+    if(!external && pll.isEnabled()) {
+      throw new IllegalArgumentException(this + " cannot be used with PLL");
+    }
+    byte mode = base;
+    if(pll.isEnabled()) {
+      mode |= 0x40;
+      mode += pll.getValue();
+    }
+    return mode;
+  }
+  
+  public int derive(int inputFrequency) {
+    return inputFrequency;
+  }
+}
---
+++ src/propasm/model/PllMode.java
@@ -0,0 +1,60 @@
+// Copyright (C) 2006 Cliff L. Biffle.
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+package propasm.model;
+
+/**
+ * @author cbiffle
+ *
+ */
+public enum PllMode {
+  PLL_DISABLED {
+    public boolean isEnabled() {
+      return false;
+    }
+  },
+  X1(1, 1),
+  X2(2, 2),
+  X4(3, 4),
+  X8(4, 8),
+  X16(5, 16);
+  
+  private final int value;
+  private final int multiplier;
+  
+  private PllMode() {
+    this(0, 1);
+  }
+  private PllMode(int value, int multiplier) {
+    this.value = value;
+    this.multiplier = multiplier;
+  }
+  
+  public int getValue() {
+    return value;
+  }
+  
+  public int getMultiplier() {
+    return multiplier;
+  }
+  
+  public boolean isEnabled() {
+    return true;
+  }
+  
+  public int multiply(int clockFrequency) {
+    return clockFrequency * multiplier;
+  }
+}
